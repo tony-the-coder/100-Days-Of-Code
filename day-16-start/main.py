bcd142eab46595a5b52a1bd55b9fb1fd38544353
Tony Aldea <tonythecoder@outlook.com>
2024-06-09 18:42:09 -0400

    first commit

---
+++ day-16-start/main.py
@@ -0,0 +1,26 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+money_machine.report()
+
+coffe_maker = CoffeeMaker()
+coffe_maker.report()
+menu = Menu()
+
+is_on = True
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like?({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffe_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffe_maker.is_resource_sufficient(drink) and money_machine.make_payment(
+            drink.cost
+        ):
+            coffe_maker.make_coffee(drink)
