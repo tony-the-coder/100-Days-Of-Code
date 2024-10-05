5acbc0598f28793a58e32d456f7a13002bb21869
Tony Aldea <tonythecoder@outlook.com>
2024-10-05 15:23:24 -0400

    Adding more

---
+++ day-17-start/game_files/question_model.py
@@ -0,0 +1,7 @@
+import main
+
+
+class Question:
+    def __init__(self, q_text, q_answer) -> None:
+        self.text = q_text
+        self.answer = q_answer
---
+++ day-17-start/game_files/quiz_brain.py
@@ -0,0 +1,9 @@
+class QuizBrain:
+    def __init__(self, q_list) -> None:
+        self.question_number = 0
+        self.question_list = q_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        input(f"{self.question_number}:{current_question.text} )True or False")
